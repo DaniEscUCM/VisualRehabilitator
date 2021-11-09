2133fdb887a6815f3374183a43956909b898d8dc
Maria <57766253+Marinu07@users.noreply.github.com>
2021-11-09 17:32:03 +0100

    Base Extendida
    Se le añadío la pantalla de exercises y de settings aunque están bacías

---
+++ app/src/main/java/com/macularehab/ExercisesActivity.java
@@ -0,0 +1,28 @@
+package com.macularehab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class ExercisesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_exercises);
+        ImageButton button = (ImageButton) findViewById(R.id.imageButton_back_exerc);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Close(v);
+            }
+        });
+    }
+    public void Close(View view){
+        finish();
+    }
+}
