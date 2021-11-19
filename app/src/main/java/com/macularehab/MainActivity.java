0441c1e37e5cf2a1f47047e07518e88043b4562e
Diego <alrp.diego@gmail.com>
2021-11-19 20:17:19 +0100

    Terminado Unica Aparicion

--- app/src/main/java/com/macularehab/MainActivity.java
+++ app/src/main/java/com/macularehab/MainActivity.java
@@ -1,8 +1,11 @@
 package com.macularehab;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
@@ -35,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
         //PATIENT
         Button dataButton = (Button) findViewById(R.id.dataButton);
+
+        //askFirstTime();
     }
 
     public void Settings(View view){
@@ -52,5 +57,35 @@ public class MainActivity extends AppCompatActivity {
 
         Intent i = new Intent(this, PatientLogin.class);
         startActivity(i);
+    }
+
+    private void askFirstTime() {
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        boolean firstStart = prefs.getBoolean("firstStart", true);
+
+        if (firstStart) {
+
+            showFirstStart();
+        }
+    }
+
+    private void showFirstStart() {
+
+        new AlertDialog.Builder(this)
+                .setTitle("First Time Dialog")
+                .setMessage("This only be show once")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create().show();
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("firstStart", false);
+        editor.apply();
     }
 }
--- app/src/main/java/com/macularehab/login/MainLogin.java
+++ app/src/main/java/com/macularehab/login/MainLogin.java
@@ -1,4 +1,15 @@
 package com.macularehab.login;
 
-public class MainLogin {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainLogin extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
