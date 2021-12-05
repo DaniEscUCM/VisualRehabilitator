c7d46fdc28d8cef5c3327168e7799e1299ad05a0
Diego Alejandro Rodriguez Pereira <alrp.diego@gmail.com>
2021-12-05 20:18:55 +0100

    Tocando Firebase

--- app/src/main/java/com/macularehab/PatientLogin.java
+++ app/src/main/java/com/macularehab/PatientLogin.java
@@ -1,12 +1,9 @@
 package com.macularehab;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.View;
 
 public class PatientLogin extends AppCompatActivity  {
 
@@ -14,6 +11,6 @@ public class PatientLogin extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        setContentView(R.layout.activity_patient_login);
+        setContentView(R.layout.activity_login_patient);
     }
 }
