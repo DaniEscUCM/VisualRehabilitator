952a94064d952e23d1bcd58f1c86f48d40511542
DiegoARP <alrp.diego@gmail.com>
2022-03-06 10:56:35 +0100

    Not Done. Create Patient Name

--- app/src/main/java/com/macularehab/patient/Patient.java
+++ app/src/main/java/com/macularehab/patient/Patient.java
@@ -1,8 +1,141 @@
 package com.macularehab.patient;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Patient {
 
     private String date;
     private String name;
+    private String first_lastName;
+    private String second_lastName;
     private String gender;
+    private String date_of_birth;
+    private String diagnostic;
+    private float av;
+    private float cv;
+    private String observations;
+
+    /*** visual difficulties in children and adults ***/
+    private boolean read;
+    private boolean write;
+    private boolean use_phone;
+    private boolean whiteboard;
+    private boolean job_difficulties;
+    private boolean face_recognition;
+    private boolean see_tv;
+    private boolean move_interior;
+    private boolean move_exterior;
+    private boolean interior_to_exterior;
+    private boolean money_management;
+    private boolean feeding;
+    private boolean home_activities;
+    private boolean shopping;
+    private boolean eating;
+    private boolean personal_care;
+
+    /*** visual behaviour ***/
+    private boolean head_movement;
+    private boolean tv_closeness;
+    private boolean specific_illumination;
+    private boolean sun_bother;
+
+    /*** use of optics and no optics help ***/
+    private boolean conventional_glasses;
+    private boolean uses_sunglasses;
+    private boolean glasses_are_useful;
+
+    /*** reading ***/
+    private boolean used_to_read_more;
+    private boolean wants_to_read_more;
+
+    private ArrayList<Boolean> checkBox;
+
+    public Patient() {
+
+        checkBox = new ArrayList<>(25);
+        for (int i = 0; i < 25; i++) checkBox.add(false);
+    }
+
+    /**** Getters And Setters ****/
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirst_lastName() {
+        return first_lastName;
+    }
+
+    public void setFirst_lastName(String first_lastName) {
+        this.first_lastName = first_lastName;
+    }
+
+    public String getSecond_lastName() {
+        return second_lastName;
+    }
+
+    public void setSecond_lastName(String second_lastName) {
+        this.second_lastName = second_lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(String date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getDiagnostic() {
+        return diagnostic;
+    }
+
+    public void setDiagnostic(String diagnostic) {
+        this.diagnostic = diagnostic;
+    }
+
+    public float getAv() {
+        return av;
+    }
+
+    public void setAv(float av) {
+        this.av = av;
+    }
+
+    public float getCv() {
+        return cv;
+    }
+
+    public void setCv(float cv) {
+        this.cv = cv;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
 }
--- app/src/main/java/com/macularehab/professional/ProfessionalCreateNewPatient.java
+++ app/src/main/java/com/macularehab/professional/ProfessionalCreateNewPatient.java
@@ -1,18 +1,61 @@
 package com.macularehab.professional;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.macularehab.R;
+import com.macularehab.patient.Patient;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 public class ProfessionalCreateNewPatient extends AppCompatActivity {
 
-    private Spinner userGender_spinner;
+    private EditText input_patient_date;
+    private EditText input_patient_name;
+    private EditText input_patient_first_last_name;
+    private EditText input_patient_second_last_name;
+    private Spinner input_patient_gender;
+    private EditText input_patient_date_of_birth;
+    private EditText input_patient_diagnostic;
+    private EditText input_patient_av;
+    private EditText input_patient_cv;
+    private EditText input_patient_observations;
+    private Button button_continue;
+
+    private String date;
+    private String name;
+    private String first_last_name;
+    private String second_last_name;
+    private String gender;
+    private String date_of_birth;
+    private String diagnostic;
+    private float av;
+    private float cv;
+    private String observations;
+
+    private Patient patient;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private int numericCode;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,6 +63,166 @@ public class ProfessionalCreateNewPatient extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_professional_create_new_patient);
         //setContentView(R.layout.activity_professional_new_patient_difficulties);
+
+        firebaseDatabase = FirebaseDatabase.getInstance("https://macularehab-default-rtdb.europe-west1.firebasedatabase.app");
+        databaseReference = firebaseDatabase.getReference();
+
+        input_patient_date = findViewById(R.id.input_create_patient_todays_date);
+        input_patient_name = findViewById(R.id.input_create_patient_name);
+        input_patient_first_last_name = findViewById(R.id.input_create_patient_first_last_name);
+        input_patient_second_last_name = findViewById(R.id.input_create_patient_second_last_name);
+        input_patient_gender = findViewById(R.id.spinner_input_create_patient_gender);
+        input_patient_date_of_birth = findViewById(R.id.input_create_patient_date_of_birth);
+        input_patient_diagnostic = findViewById(R.id.input_create_patient_diagnostic);
+        input_patient_av = findViewById(R.id.input_create_patient_av);
+        input_patient_cv = findViewById(R.id.input_create_patient_cv);
+        input_patient_observations = findViewById(R.id.input_create_patient_observations);
+
+        button_continue = findViewById(R.id.button_create_patient_continue);
+        button_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonContinueClicked();
+            }
+        });
+
+        setDate();
     }
 
+    private void setDate() {
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        input_patient_date.setText(formatter.format(date));
+
+        Log.d("Date", "default date " + date.toString());
+    }
+
+    private void buttonContinueClicked() {
+
+        readInputs();
+    }
+
+    private void readInputs() {
+
+        date = input_patient_date.getText().toString();
+        Log.d("Date", "after read date " + date);
+        name = input_patient_name.getText().toString();
+        first_last_name = input_patient_first_last_name.getText().toString();
+        second_last_name = input_patient_second_last_name.getText().toString();
+        gender = input_patient_gender.getSelectedItem().toString();
+        date_of_birth = input_patient_date_of_birth.getText().toString();
+        diagnostic = input_patient_diagnostic.getText().toString();
+        av = Float.parseFloat(input_patient_av.getText().toString());
+        Log.d("AV", String.valueOf(av));
+        cv = Float.parseFloat(input_patient_cv.getText().toString());
+    }
+
+    //TODO utilizar string.xml y tambien falta comprobar que no exista un usuario con el mismo nombre
+    private void validateInputs() {
+
+        boolean all_correct = true;
+
+        if (date.equals("")) {
+            input_patient_date.setError("required");
+            all_correct = false;
+        }
+        if (name.equals("")) {
+            input_patient_name.setError("required");
+            all_correct = false;
+        }
+        if (first_last_name.equals("")) {
+            input_patient_first_last_name.setError("required");
+            all_correct = false;
+        }
+        if (second_last_name.equals("")) {
+            input_patient_second_last_name.setError("required");
+            all_correct = false;
+        }
+        if (date_of_birth.equals("")) {
+            input_patient_date_of_birth.setError("required");
+            all_correct = false;
+        }
+        if (diagnostic.equals("")) {
+            input_patient_diagnostic.setError("required");
+            all_correct = false;
+        }
+        if (av == 0) {
+            input_patient_av.setError("required");
+            all_correct = false;
+        }
+        if (cv == 0) {
+            input_patient_cv.setError("required");
+            all_correct = false;
+        }
+
+        if (all_correct) {
+            createPatient();
+        }
+        else {
+            showAlertMissingData();
+        }
+    }
+
+    private void showAlertMissingData() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("There are some fields that ar not filled up")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void createPatient() {
+
+        patient = new Patient();
+        patient.setDate(date);
+        patient.setName(name);
+        patient.setFirst_lastName(first_last_name);
+        patient.setSecond_lastName(second_last_name);
+        patient.setGender(gender);
+        patient.setDate_of_birth(date_of_birth);
+        patient.setDiagnostic(diagnostic);
+        patient.setAv(av);
+        patient.setCv(cv);
+        patient.setObservations(observations);
+
+    }
+
+    private void addPatientToDataBase() {
+
+
+    }
+
+    private void generateNumericCode() {
+
+        int min = 11111112;
+        int max = 99999998;
+        Random random = new Random();
+        numericCode = random.nextInt(max-min) + min;
+    }
+
+    private void checkNumericCode() {
+
+        databaseReference.child(String.valueOf(numericCode))
+                .get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+
+                    String value = String.valueOf(task.getResult().getValue());
+                    if (!value.equals("null")) {
+                        generateNumericCode();
+                    }
+                }
+            }
+        });
+    }
 }
