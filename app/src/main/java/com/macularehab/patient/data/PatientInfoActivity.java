d1142be70b32da51b90624b4dc3758a258299f9d
DiegoARP <alrp.diego@gmail.com>
2022-03-29 16:13:09 +0200

    Removed Patient Info Activity

--- app/src/main/java/com/macularehab/patient/data/PatientInfoActivity.java
+++ app/src/main/java/com/macularehab/patient/data/PatientInfoActivity.java
@@ -7,12 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.macularehab.R;
 
-public class PatientHomeData extends AppCompatActivity {
+public class PatientInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        setContentView(R.layout.activity_patient_home_choose_data_exercise);
+        setContentView(R.layout.activity_professional_patient_info);
     }
 }
